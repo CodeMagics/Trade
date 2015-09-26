9a14e0e5eed965c9751c7665c1a0f886b2b1cf30
20131302 <643087116@qq.com>
2015-09-26 16:04:30 +0800

    注释

--- src/com/skyfin/servlet/Login.java
+++ src/com/skyfin/servlet/Login.java
@@ -30,20 +30,7 @@ public class Login extends HttpServlet {
 		super.destroy();
 	}
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 * 
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request
-	 *            the request send by the client to the server
-	 * @param response
-	 *            the response send by the server to the client
-	 * @throws ServletException
-	 *             if an error occurred
-	 * @throws IOException
-	 *             if an error occurred
-	 */
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
